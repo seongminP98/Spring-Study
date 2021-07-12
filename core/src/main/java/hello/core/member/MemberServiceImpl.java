d418fa55df9462bc517a888366e8b37d222eafb7
seongmin <psm7335@gmail.com>
2021-07-12 16:52:46 +0900

    컴포넌트 스캔

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,9 +1,15 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements  MemberService{
 
     private final MemberRepository memberRepository;
 
+    @Autowired //자동 의존관계 주입. 생성자에 붙여주면 MemberRepository에 맞는 애를 찾아와서 자동으로 주입해준다.
+    //마치 ac.getBean(MemberRepository.class) 처럼 동작
     public MemberServiceImpl(MemberRepository memberRepository) { //DI (의존관계 주입)
         this.memberRepository = memberRepository;
     }
