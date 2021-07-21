743cd65908a4efe556d010285e5f6ea564cc2d01
seongmin <psm7335@gmail.com>
2021-07-21 23:20:07 +0900

    단순하고 실용적인 컨트롤러 - v4

--- servlet/src/main/java/hello/servlet/web/frontcontroller/v4/controller/MemberSaveControllerV4.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v4/controller/MemberSaveControllerV4.java
@@ -1,26 +1,28 @@
-package hello.servlet.web.frontcontroller.v3.controller;
+package hello.servlet.web.frontcontroller.v4.controller;
 
 import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
 import hello.servlet.web.frontcontroller.ModelView;
-import hello.servlet.web.frontcontroller.v3.ControllerV3;
+import hello.servlet.web.frontcontroller.v4.ControllerV4;
 
 import java.util.Map;
 
-public class MemberSaveControllerV3 implements ControllerV3 {
+public class MemberSaveControllerV4 implements ControllerV4 {
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
     @Override
-    public ModelView process(Map<String, String> paramMap) {
+    public String process(Map<String, String> paramMap, Map<String, Object> model) {
+        //paramMap에서 값 꺼냄.
         String username = paramMap.get("username");
         int age = Integer.parseInt(paramMap.get("age"));
 
+        //내 비즈니스 로직 실행
         Member member = new Member(username, age);
         memberRepository.save(member);
 
-        ModelView mv = new ModelView("save-result");
-        mv.getModel().put("member",member);
-        return mv;
+        //모델에 값 넣어주고 리턴.
+        model.put("member", member);
+        return "save-result";
     }
 }
