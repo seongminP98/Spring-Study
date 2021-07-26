aef5bbf2e9b1205c488963950b54ec1c625751df
seongmin <psm7335@gmail.com>
2021-07-26 18:03:08 +0900

    상품 목록 - 타임리프

---
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,48 @@
+package hello.itemservice.web.basic;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor  //롬복에 있음. 이걸 사용하면 final이 붙은걸 가지고 생성자를 만들어줌.
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    /*
+ @RequiredArgsConstructor 있기 때문에 생성자 없어도됨.
+
+    //@Autowired    //생성자가 하나만 있으면 생략 가능.
+    public BasicItemController(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+     */
+
+    @GetMapping
+    public String items(Model model) {
+
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    /**
+     * 테스트용 데이터 추가
+     */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA",10000,10));
+        itemRepository.save(new Item("itemB",20000,20));
+    }
+}
