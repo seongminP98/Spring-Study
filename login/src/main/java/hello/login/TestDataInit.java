f970a97f8ed778a8fd885a30f2b0361aba9f2d05
seongmin <psm7335@gmail.com>
2021-08-10 18:30:02 +0900

    회원 가입

--- login/src/main/java/hello/login/TestDataInit.java
+++ login/src/main/java/hello/login/TestDataInit.java
@@ -2,6 +2,8 @@ package hello.login;
 
 import hello.login.domain.item.Item;
 import hello.login.domain.item.ItemRepository;
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -12,14 +14,23 @@ import javax.annotation.PostConstruct;
 public class TestDataInit {
 
     private final ItemRepository itemRepository;
+    private final MemberRepository memberRepository;
 
     /**
      * 테스트용 데이터 추가
      */
-    @PostConstruct
+    @PostConstruct //의존성 주입이 이루어진 후 초기화를 수행하는 메서드이다. service를 수행하기 전에 발생한다.
     public void init() {
         itemRepository.save(new Item("itemA", 10000, 10));
         itemRepository.save(new Item("itemB", 20000, 20));
+
+        Member member = new Member();
+        member.setLoginId("test");
+        member.setPassword("test!");
+        member.setName("테스터");
+
+        memberRepository.save(member);
+
     }
 
 }
