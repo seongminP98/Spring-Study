b7e7eed07b26de135d46c3928c92b54505b40fd4
seongmin <psm7335@gmail.com>
2021-08-16 16:53:43 +0900

    ControllerAdvice

--- exception/src/main/java/hello/exception/exhandler/advice/ExControllerAdvice.java
+++ exception/src/main/java/hello/exception/exhandler/advice/ExControllerAdvice.java
@@ -1,19 +1,17 @@
-package hello.exception.api;
+package hello.exception.exhandler.advice;
 
 import hello.exception.exception.UserException;
 import hello.exception.exhandler.ErrorResult;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
-@RestController
-public class ApiExceptionV2Controller {
-    /*ExControllerAdvice 에서 처리하도록 바꿈.
-    //ExceptionHandler는 이 컨트롤러 안에서만 적용됨. 다른 컨트롤러랑 상관없음.
+@RestControllerAdvice
+public class ExControllerAdvice {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST) //이걸 안넣어주면 status code 가 200이 됨.
     @ExceptionHandler(IllegalArgumentException.class) //이 컨트롤러에서 IllegalArgumentException 예외가 발생하면 얘가 잡고 이 로직이 호출이됨.
@@ -34,32 +32,5 @@ public class ApiExceptionV2Controller {
     public ErrorResult exHandler(Exception e) { //최상위 예외이므로 처리되지 못한 예외들이 다 여기서 처리됨. 자식까지 잡아줌.
         log.error("[exceptionHandler] ex", e);
         return new ErrorResult("EX", "내부 오류");
-    }
-
-     */
-
-    @GetMapping("/api2/members/{id}")
-    public MemberDto getMember(@PathVariable("id") String id) {
-
-        if(id.equals("ex")) {
-            throw new RuntimeException("잘못된 사용자");
-        }
-
-        if(id.equals("bad")) {
-            throw new IllegalArgumentException("잘못된 입력 값");
-        }
-
-        if(id.equals("user-ex")) {
-            throw new UserException("사용자 오류");
-        }
-
-        return new MemberDto(id, "hello " + id);
-    }
-
-    @Data
-    @AllArgsConstructor
-    static class MemberDto {
-        private String memberId;
-        private String name;
     }
 }
