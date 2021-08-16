fbff9dc26219fb63689867dd017ebf3afc71aa87
seongmin <psm7335@gmail.com>
2021-08-16 16:43:59 +0900

    @ExceptionHandler

---
+++ exception/src/main/java/hello/exception/api/ApiExceptionV2Controller.java
@@ -0,0 +1,62 @@
+package hello.exception.api;
+
+import hello.exception.exception.UserException;
+import hello.exception.exhandler.ErrorResult;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+public class ApiExceptionV2Controller {
+    //ExceptionHandler는 이 컨트롤러 안에서만 적용됨. 다른 컨트롤러랑 상관없음.
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST) //이걸 안넣어주면 status code 가 200이 됨.
+    @ExceptionHandler(IllegalArgumentException.class) //이 컨트롤러에서 IllegalArgumentException 예외가 발생하면 얘가 잡고 이 로직이 호출이됨.
+    public ErrorResult illegalExHandler(IllegalArgumentException e) {
+        log.error("[exceptionHandler] ex", e);
+        return new ErrorResult("BAD", e.getMessage());
+    }
+
+    @ExceptionHandler//(UserException) 생략가능. 메서드 파라미터의 예외가 지정됨.
+    public ResponseEntity<ErrorResult> userExHandler(UserException e) {
+        log.error("[exceptionHandler] ex", e);
+        ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
+        return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler
+    public ErrorResult exHandler(Exception e) { //최상위 예외이므로 처리되지 못한 예외들이 다 여기서 처리됨. 자식까지 잡아줌.
+        log.error("[exceptionHandler] ex", e);
+        return new ErrorResult("EX", "내부 오류");
+    }
+
+    @GetMapping("/api2/members/{id}")
+    public MemberDto getMember(@PathVariable("id") String id) {
+
+        if(id.equals("ex")) {
+            throw new RuntimeException("잘못된 사용자");
+        }
+
+        if(id.equals("bad")) {
+            throw new IllegalArgumentException("잘못된 입력 값");
+        }
+
+        if(id.equals("user-ex")) {
+            throw new UserException("사용자 오류");
+        }
+
+        return new MemberDto(id, "hello " + id);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String memberId;
+        private String name;
+    }
+}
