8a5490341c28e99212846f55d81f11ce5d5f5969
박성민 <psm7335@naver.com>
2022-04-20 21:22:40 +0900

    JDBC 개발 - 조회

--- jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,19 +1,32 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
 
     @Test
     void crud() throws SQLException {
-        Member member = new Member("memberV1", 10000);
+        //save
+        Member member = new Member("memberV4", 10000);
         repository.save(member);
+
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        // Member 에서 lombok 의 @Data 사용.
+        log.info("member != findMember {}", member == findMember); //false
+        log.info("member equals findMember {}", member.equals(findMember)); //true
+        assertThat(findMember).isEqualTo(member);
     }
 }
