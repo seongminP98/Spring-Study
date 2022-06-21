310a1798edefd755c48f3d4fa02ba6b747dd3764
박성민 <psm7335@naver.com>
2022-06-21 22:47:31 +0900

    예외 포함과 스택 트레이스

--- jdbc/src/test/java/hello/jdbc/exception/basic/UnCheckedAppTest.java
+++ jdbc/src/test/java/hello/jdbc/exception/basic/UnCheckedAppTest.java
@@ -1,5 +1,6 @@
 package hello.jdbc.exception.basic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.net.ConnectException;
@@ -10,6 +11,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 /**
  * 런타임 예외로 예외 변환.
  */
+@Slf4j
 public class UnCheckedAppTest {
 
     @Test
@@ -19,10 +21,20 @@ public class UnCheckedAppTest {
                 .isInstanceOf(Exception.class);
     }
 
+    @Test
+    void printEx() {
+        Controller controller = new Controller();
+        try {
+            controller.request();
+        } catch (Exception e) {
+            log.info("ex", e); // 로그 출력 시 마지막 파라미터에 예외를 넣어주면 스택 트레이스 출력 가능
+        }
+    }
+
     static class Controller {
         Service service = new Service();
 
-        public void request() throws SQLException, ConnectException {
+        public void request() {
             service.logic();
         }
     }
@@ -47,7 +59,7 @@ public class UnCheckedAppTest {
         public void call() {
             try {
                 runSQL();
-            } catch (SQLException e) {
+            } catch (SQLException e) { // 예외를 전환할 때 기존 예외를 넣어주자!!
                 throw new RuntimeSQLException(e);
             }
         }
