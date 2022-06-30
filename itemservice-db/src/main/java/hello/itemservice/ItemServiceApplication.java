f30db8d94e0f96a30798e9d66588c02453ad661c
박성민 <psm7335@naver.com>
2022-06-30 23:11:33 +0900

    스프링 DB 2편 - 데이터 접근 활용 기술

---
+++ itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -0,0 +1,26 @@
+package hello.itemservice;
+
+import hello.itemservice.config.*;
+import hello.itemservice.repository.ItemRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
+
+
+@Import(MemoryConfig.class)
+@SpringBootApplication(scanBasePackages = "hello.itemservice.web") // 이 하위에 있는 것들만 컴포넌트 스캔하도록. service와 repository는 바꿔가면서 할거라서.
+public class ItemServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemServiceApplication.class, args);
+	}
+
+	@Bean
+	@Profile("local")
+	public TestDataInit testDataInit(ItemRepository itemRepository) {
+		return new TestDataInit(itemRepository);
+	}
+
+}
